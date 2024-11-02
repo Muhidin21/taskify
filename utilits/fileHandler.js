95ebecabff9fbc6ce65db416a9f111a9496ef51b
unknown <iameyman00@gmail.com>
2024-11-02 18:33:55 +0300

    fileHandler.js

--- utilits/fileHandler.js
+++ utilits/fileHandler.js
@@ -1,3 +1,4 @@
+const { execFileSync } = require('child_process');
 const fs=require('fs');
 const path=require('path');
 const filepath='.data/task.json';
@@ -5,5 +6,9 @@ const writeTasksToFile=(tasks)=>{
     fs.writeFileSync(filepath,JSON.stringify(tasks))
 }
 const readTasksForfile=(tasks)=>{
-    
+    if(! fs.existsSync(filepath)){
+        writeTasksToFile([])
+    }
+    const data=fs.readFileSync(filepath);
+    return JSON.parse(data)
 }
